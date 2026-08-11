8115302e73863b51f960d5bf5e2876ac912b5789
Sebastian Vansteenkiste <sebavanten@gmail.com>
2026-08-11 14:28:53 +0000

    Resolved unrelated warnings

--- src/CmsWebhook/CmsWebhook.Domain/Class1.cs
+++ src/CmsWebhook/CmsWebhook.Domain/Class1.cs
@@ -1,5 +1,8 @@
 ﻿namespace CmsWebhook.Domain;
 
+/// <summary>
+/// Placeholder class created by project scaffolding; removed or replaced as domain types are added.
+/// </summary>
 public class Class1
 {
 
